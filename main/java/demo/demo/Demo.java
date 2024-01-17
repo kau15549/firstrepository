ad149fb3e7a44ae05b22746e92e9ab5fa92ee0e0
kau15549 <harpreetsidhu1205@gmail.com>
2024-01-17 09:22:18 -0500

    COMMIT

---
+++ main/java/demo/demo/Demo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package demo.demo;
+
+/**
+ *
+ * @author User
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
